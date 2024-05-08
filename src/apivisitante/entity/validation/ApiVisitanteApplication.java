4a7ec6c44dab78f10034ddb209a4d1daaaaca0c6
Neo40 <tron36.cc@gmail.com>
2024-05-08 17:50:09 -0300

    Adiçao de validation

---
+++ src/apivisitante/entity/validation/ApiVisitanteApplication.java
@@ -0,0 +1,13 @@
+package apivisitante.entity.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVisitanteApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiVisitanteApplication.class, args);
+    }
+}
